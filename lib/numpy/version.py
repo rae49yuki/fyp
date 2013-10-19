3ac93b61a42cc9cbd332c9f5dcc702ca6a8c8931
CHEN XI <hp_cx@hotmail.com>
2013-10-19 23:13:43 +0800

    imported PIL library

---
+++ lib/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.7.1'
+version = '1.7.1'
+full_version = '1.7.1'
+git_revision = '971bab3d51726b95f5afe0c22cbbd7983023f626'
+release = True
+
+if not release:
+    version = full_version
